f52731999270fa45f3d015ab60581aee19adfe5c
unknown <contact@mohamed-alami.com>
2022-12-09 15:59:46 +0100

    Form Rx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-rx';
+
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(10)
+      ]], // Valeur par défaut
+      email: ['',
+        [
+          Validators.required,
+          Validators.email
+        ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(10),      
+      ]],
+      phone: ['', [
+        Validators.required,
+        Validators.minLength(9),
+        Validators.maxLength(10),
+        Validators.pattern('^[0-9]*$')
+      ]],
+      terms: ['', [
+        Validators.requiredTrue
+      ]]
+    });
+  }
+
+  get username() {
+    return this.registerForm.get('username');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get phone() {
+    return this.registerForm.get('phone');
+  }
+
+  get terms() {
+    return this.registerForm.get('terms');
+  }
+  submit() {
+    if (!this.registerForm.valid) {
+      alert('Form is invalid')
+      return;
+    }
+
+    alert('Success');
+  }
 }
